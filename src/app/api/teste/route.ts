a5725a176fbb5e69514310f4043edea50fb542ee
n7thz <nathanferreiradev@gmail.com>
2025-09-08 04:03:32 -0300

    fix: remoção de fakes/js

--- src/app/api/teste/route.ts
+++ src/app/api/teste/route.ts
@@ -1,41 +1,41 @@
-import { prisma } from "@/lib/prisma"
-import { faker } from "@faker-js/faker"
-import { NextResponse } from "next/server"
-import { Notification } from "@prisma/client"
+// import { prisma } from "@/lib/prisma"
+// import { faker } from "@faker-js/faker"
+// import { NextResponse } from "next/server"
+// import { Notification } from "@prisma/client"
 
-export async function GET() {
-  const notifications: Notification[] = []
+// export async function GET() {
+//   const notifications: Notification[] = []
 
-  // Usar Promise.all para executar todas as criações em paralelo
-  await Promise.all(
-    Array.from({ length: 8 }).map(async () => {
-      const action = faker.helpers.arrayElement([
-        "CREATE",
-        "UPDATE",
-        "DELETE",
-        "MIN_QUANTITY",
-      ])
+//   // Usar Promise.all para executar todas as criações em paralelo
+//   await Promise.all(
+//     Array.from({ length: 8 }).map(async () => {
+//       const action = faker.helpers.arrayElement([
+//         "CREATE",
+//         "UPDATE",
+//         "DELETE",
+//         "MIN_QUANTITY",
+//       ])
 
-      // Se action for "MIN_QUANTITY", read sempre será false
-      // Caso contrário, pode ser true ou false aleatoriamente
-      const read = action === "MIN_QUANTITY" ? false : faker.datatype.boolean()
+//       // Se action for "MIN_QUANTITY", read sempre será false
+//       // Caso contrário, pode ser true ou false aleatoriamente
+//       const read = action === "MIN_QUANTITY" ? false : faker.datatype.boolean()
 
-      const notificationObject = {
-        id: faker.string.uuid(),
-        name: faker.lorem.words(3),
-        description: faker.datatype.boolean() ? faker.lorem.sentence() : null,
-        createdAt: faker.date.recent(),
-        read: read, // Aqui aplicamos a regra
-        action: action,
-      }
+//       const notificationObject = {
+//         id: faker.string.uuid(),
+//         name: faker.lorem.words(3),
+//         description: faker.datatype.boolean() ? faker.lorem.sentence() : null,
+//         createdAt: faker.date.recent(),
+//         read: read, // Aqui aplicamos a regra
+//         action: action,
+//       }
 
-      const notification = await prisma.notification.create({
-        data: notificationObject,
-      })
+//       const notification = await prisma.notification.create({
+//         data: notificationObject,
+//       })
 
-      notifications.push(notification)
-    })
-  )
+//       notifications.push(notification)
+//     })
+//   )
 
-  return NextResponse.json(notifications)
-}
+//   return NextResponse.json(notifications)
+// }
